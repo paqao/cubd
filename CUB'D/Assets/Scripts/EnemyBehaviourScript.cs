38c29075d7054fc9fcd18655639f08bde6d7445e
Pawel Labno <pacco.inter@gmail.com>
2017-05-20 13:41:30 +0200

    enemies

--- CUB'D/Assets/Scripts/EnemyBehaviourScript.cs
+++ CUB'D/Assets/Scripts/EnemyBehaviourScript.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class EnemyBehaviourScript : MonoBehaviour {
 
-	// Use this for initialization
+    public double direction = 1.0f;
+    public double speed = 5.0f;
+    public double x = 0.0f;
+    
+    // Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
+        x = direction * Time.deltaTime * speed;
+
+        transform.Translate(new Vector3((float) x, 0.0f, 0.0f), Space.World);
 	}
+
+    void OnCollisionEnter(Collision collision){
+        direction *= -1.0f;
+    }
 }
