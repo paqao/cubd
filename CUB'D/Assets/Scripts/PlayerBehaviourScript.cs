4ba6fbb5f6e85851fcdae979e849c7a3c062cb60
RayosPL <kras.kryspin@gmail.com>
2017-05-20 15:11:27 +0200

    tweaning playera

--- CUB'D/Assets/Scripts/PlayerBehaviourScript.cs
+++ CUB'D/Assets/Scripts/PlayerBehaviourScript.cs
@@ -10,8 +10,14 @@ public class PlayerBehaviourScript : MonoBehaviour {
     public bool doubleJumpEnabled;
 
     private bool doubleJumpState;
+	private bool isJumping;
 	private bool canMoveVertically = true;
-	private float x = 0, y = 0;
+	private float x, y;
+
+	private bool scaling = true;
+
+	private int timer = 0;
+	private bool returnToNormal = false;
     void Start () {
 		
 		x = 0; y = 0;
@@ -29,6 +35,8 @@ public class PlayerBehaviourScript : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GetComponent<Rigidbody>().AddForce(0, jumpPower, 0);
+			isJumping = true;
+			returnToNormal = false;
         }
 
         // logic
@@ -38,5 +46,39 @@ public class PlayerBehaviourScript : MonoBehaviour {
 		}
 		else
 			canMoveVertically = true; //3D
+
+		Jumping ();
+
+
+
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.tag == "Floor" && timer > 5) {
+			isJumping = false;
+			timer = 0;
+
+
+		}
+	}
+
+	void Jumping ()
+	{
+		if (isJumping) {
+			timer++;
+			transform.localScale = Vector3.Lerp (transform.localScale, new Vector3 (0.75f, 1.25f, 0.75f), 0.5f);
+		}
+		else
+			if (!isJumping && !returnToNormal) {
+				if (transform.localScale.x < 1.25f) {
+					transform.localScale = Vector3.Lerp (transform.localScale, new Vector3 (1.25f, 0.75f, 1.25f), 0.5f);
+				}
+				if (transform.localScale.x > 1.22f)
+					returnToNormal = true;
+			}
+		if (returnToNormal) {
+			transform.localScale = Vector3.Lerp (transform.localScale, new Vector3 (1f, 1f, 1f), 0.75f);
+		}
 	}
 }
