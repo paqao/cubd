4d96cc4b0724b7c6ce1fef673002660857f7c856
Pawel Labno <pawel.labno@outlook.com>
2017-05-20 11:28:34 +0200

    cos

---
+++ CUB'D/Assets/Scripts/PlayerBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviourScript : MonoBehaviour {
+
+    // Use this for initialization
+    public float speed;
+    public float jumpPower;
+    public bool doubleJumpEnabled;
+
+    private bool doubleJumpState;
+
+    void Start () {
+		
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // input
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Translate(x, 0, y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(0, jumpPower, 0);
+        }
+
+        // logic
+	}
+}
